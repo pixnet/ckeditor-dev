7ee95d345b957aae8e294dccb38735b67845b808
yvonneyu <yvonneyu@pixnet.tw>
2016-03-21 18:33:05 +0800

    增加先判斷圖片的 orientation 後，在決定圖片寬高的部份 避免 orientation 轉向的圖片破圖

--- plugins/uploadimage/plugin.js
+++ plugins/uploadimage/plugin.js
@@ -57,15 +57,71 @@
 				},
 
 				onUploaded: function( upload ) {
-					var sizes = getNormalImgSize(this.parts.img.$.naturalWidth, this.parts.img.$.naturalHeight);
-					// Set width and height to prevent blinking.
-					this.replaceWith( '<img src="' + upload.url + '" ' +
-						'width="' + sizes.width + '" ' +
-						'height="' + sizes.height + '">' );
-					// fire change event when image finish uploaded
-					editor.fire( 'change' );
+					var img = this.parts.img.$,
+						that = this;
+					// 檢查完 orientation 後確認真實的寬高後在處理設定圖片寬高的部份
+					getOrientation(upload.file, function getOrientation (orientation) {
+						var changeWH = 5 <= orientation && 8 >= orientation,
+							width = changeWH ? img.naturalHeight : img.naturalWidth,
+							height = changeWH ? img.naturalWidth : img.naturalHeight,
+							sizes = getNormalImgSize(width, height);
+						// Set width and height to prevent blinking.
+						that.replaceWith('<img src="' + upload.url + '" ' +
+							'width="' + sizes.width + '" ' +
+							'height="' + sizes.height + '">');
+						// fire change event when image finish uploaded
+						editor.fire('change');
+					});
 				}
 			});
+
+			// 參考 http://stackoverflow.com/a/32490603/2426806 答案取得圖片的 Orientation
+			// error code: not jpeg(-2), not defined(-1)
+			// 1,2,3,4 上下相反或顛倒（不影響寬高）
+			// 5,6,7,8 左右相反（影響寬高，寬高會相反）
+			function getOrientation(file, callback) {
+				// 假如沒有檔案或是不資源 FileReader，回傳 -1
+				if (!file || !window.FileReader || !window.FileReader.prototype.readAsArrayBuffer) {
+					callback(-1);
+				}
+				// 避免出錯，catch 到錯誤還是要回傳 callback
+				try {
+					var reader = new FileReader();
+					reader.onload = function(e) {
+						var view = new DataView(e.target.result);
+						// 檢查是否是 jpg (非 jpg 沒有 EXIF 資訊）
+						if (view.getUint16(0, false) != 0xFFD8) {
+							return callback(-2);
+						}
+						var length = view.byteLength,
+							offset = 2;
+						while (offset < length) {
+							var marker = view.getUint16(offset, false);
+							offset += 2;
+							if (marker == 0xFFE1) {
+								if (view.getUint32(offset += 2, false) != 0x45786966) {
+									return callback(-1);
+								}
+								var little = view.getUint16(offset += 6, false) == 0x4949;
+								offset += view.getUint32(offset + 4, little);
+								var tags = view.getUint16(offset, little);
+								offset += 2;
+								for (var i = 0; i < tags; i++) {
+									if (view.getUint16(offset + (i * 12), little) == 0x0112) {
+										return callback(view.getUint16(offset + (i * 12) + 8, little));
+									}
+								}
+							}
+							else if ((marker & 0xFF00) != 0xFF00) break;
+							else offset += view.getUint16(offset, false);
+						}
+						return callback(-1);
+					};
+					reader.readAsArrayBuffer(file.slice(0, 64 * 1024));
+				} catch (e) {
+					callback(-1);
+				}
+			}
 
 			// calculate new image size，make sure max size of image edge is 600px
 			function getNormalImgSize (oW, oH) {
